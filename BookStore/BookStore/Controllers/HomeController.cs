12d7ed63246b36c587052f42de18a388e3cb87db
Grigor Temejikyan <gtemejikyan@gmail.com>
2016-09-06 16:52:29 +0400

    Deleted About.cshtml, Contact.cshtml Change _Leyout.cshtml

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -11,20 +11,6 @@ namespace BookStore.Controllers
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        }      
     }
 }
